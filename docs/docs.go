fecd71e71bc0cdd97194769d60ecba1ecfe83943
unknown <raihan2-byte>
2023-11-20 15:42:24 +0700

    fix sweager

--- docs/docs.go
+++ docs/docs.go
@@ -162,7 +162,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
+	Version:          "",
 	Host:             "https://backend-gwf-production.up.railway.app/",
 	BasePath:         "",
 	Schemes:          []string{},
