578bc904b1bf9f8ea89f55005794c2e9f9870d9f
unknown <raihan2-byte>
2023-11-21 18:47:43 +0700

    fix env

--- database/db.go
+++ database/db.go
@@ -10,30 +10,30 @@ import (
 	"greenwelfare/gallery"
 	"greenwelfare/veganguide"
 	"greenwelfare/workshop"
+	"log"
 	"os"
 	"os/user"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDb() (*gorm.DB, error){
 
-	// if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); exists == false {
-	// 	if err := godotenv.Load(); err != nil {
-	// 		log.Fatal("error loading .env file:", err)
-	// 	}
-	// }
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); exists == false {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("error loading .env file:", err)
+		}
+	}
 	
-	dbUsername := os.Getenv("MYSQLUSER")
-	dbPassword := os.Getenv("MYSQLPASSWORD")
-	dbHost := os.Getenv("MYSQLHOST")
-	dbPort := os.Getenv("MYSQLPORT")
-	dbName := os.Getenv("MYSQLDATABASE")
-	
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", dbHost, dbUsername, dbPassword, dbName, dbPort)
-	
-		dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&d=True&loc=Local"
+		dbUsername := os.Getenv("MYSQLUSER")
+		dbPassword := os.Getenv("MYSQLPASSWORD")
+		dbHost := os.Getenv("MYSQLHOST")
+		dbPort := os.Getenv("MYSQLPORT")
+		dbName := os.Getenv("MYSQLDATABASE")
+
+		dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err != nil {
 			return nil, err
