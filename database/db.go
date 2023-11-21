36a3a8265d0b0394124d7bc64b55c7fc253e1ea5
unknown <raihan2-byte>
2023-11-21 19:05:00 +0700

    fix db

--- database/db.go
+++ database/db.go
@@ -22,20 +22,41 @@ import (
 
 func InitDb() (*gorm.DB, error){
 
-	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); exists == false {
-		if err := godotenv.Load(); err != nil {
-			log.Fatal("error loading .env file:", err)
-		}
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file:", err)
 	}
 	
-		dbUsername := os.Getenv("MYSQLUSER")
-		dbPassword := os.Getenv("MYSQLPASSWORD")
-		dbHost := os.Getenv("MYSQLHOST")
-		dbPort := os.Getenv("MYSQLPORT")
-		dbName := os.Getenv("MYSQLDATABASE")
+	dbUsername, exists := os.LookupEnv("MYSQLUSER")
+	if !exists {
+		log.Fatal("MYSQLUSER environment variable is not set")
+	}
+	
+	dbPassword, exists := os.LookupEnv("MYSQLPASSWORD")
+	if !exists {
+		log.Fatal("MYSQLPASSWORD environment variable is not set")
+	}
+	
+	dbHost, exists := os.LookupEnv("MYSQLHOST")
+	if !exists {
+		log.Fatal("MYSQLHOST environment variable is not set")
+	}
+	
+	dbPort, exists := os.LookupEnv("MYSQLPORT")
+	if !exists {
+		log.Fatal("MYSQLPORT environment variable is not set")
+	}
+	
+	dbName, exists := os.LookupEnv("MYSQLDATABASE")
+	if !exists {
+		log.Fatal("MYSQLDATABASE environment variable is not set")
+	}
+	
+	// Gunakan nilai variabel lingkungan untuk koneksi database
+	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	
 
 
-		dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+		// dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 		fmt.Println("Nilai dbUsername:", dbUsername)
 		fmt.Println("Nilai dbPassword:", dbPassword)
 		fmt.Println("Nilai dbHost:", dbHost)
