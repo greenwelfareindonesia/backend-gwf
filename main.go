16aaead57a6847143d1870dc055b825748f2fdcb
unknown <raihan2-byte>
2023-11-30 19:44:08 +0700

    fix cors

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
 		AllowAllOrigins: true,
-		AllowHeaders: []string{"Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers, Access-Control-Allow-Origin , Origin , Accept , X-Requested-With , Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"},
+		AllowHeaders: []string{"Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Origin , Accept , X-Requested-With , Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"},
 		AllowMethods: []string{"POST, OPTIONS, GET, PUT, DELETE"},
 	  }))
 
