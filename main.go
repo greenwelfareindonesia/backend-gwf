9fd27d3b6bd1169217d6d95bb0d21a7a4f430ec2
unknown <raihan2-byte>
2023-12-14 20:16:20 +0700

    fix artikel sweager

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"greenwelfare/handler"
 )
 
+// @title Sweager Service API
+// @description Sweager service API in Go using Gin framework
+// @host backend-gwf-production.up.railway.app
+// @securitydefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
 
 func main() {
 	handler.StartApp()
