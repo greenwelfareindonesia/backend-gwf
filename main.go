f28d2c29ae8c33ff3d78841f4324fd96a0f3ed6c
unknown <raihan2-byte>
2023-12-14 17:21:06 +0700

    create new sweager

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"greenwelfare/handler"
 )
 
+// @title Sweager Service API
+// @description Sweager service API in Go using Gin framework
+// @host backend-gwf-production.up.railway.app
+// @securitydefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
 func main() {
 	handler.StartApp()
 }
