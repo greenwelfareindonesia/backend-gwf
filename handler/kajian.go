04cd10a59fc9a5b938b8a676fea83272c193451e
secona <77039267+secona@users.noreply.github.com>
2024-09-02 20:50:31 +0700

    feat: kajian handler for CR

--- handler/kajian.go
+++ handler/kajian.go
@@ -1,21 +1,65 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"greenwelfare/dto"
+	"greenwelfare/helper"
+	"greenwelfare/service"
+	"net/http"
 
-type kajianHandler struct {}
+	"github.com/gin-gonic/gin"
+)
+
+type kajianHandler struct {
+	kajianService service.ServiceKajian
+}
 
 func NewKajianHandler() *kajianHandler {
 	return &kajianHandler{}
 }
 
 func (h *kajianHandler) CreateKajian(c *gin.Context) {
+	// TODO: upload images
 
+	var input dto.InputKajian
+	if err := c.ShouldBind(&input); err != nil {
+		response := helper.FailedResponse1(http.StatusUnprocessableEntity, err.Error(), err)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	kajian, err := h.kajianService.Create(input)
+	if err != nil {
+		response := helper.FailedResponse1(http.StatusBadRequest, err.Error(), err)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.SuccessfulResponse1(kajian)
+	c.JSON(http.StatusCreated, response)
 }
 
 func (h *kajianHandler) GetOneKajian(c *gin.Context) {
+	slug := c.Param("slug")
 
+	kajian, err := h.kajianService.GetOne(slug)
+	if err != nil {
+		response := helper.FailedResponse1(http.StatusBadRequest, err.Error(), err)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.SuccessfulResponse1(kajian)
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *kajianHandler) GetAllKajian(c *gin.Context) {
+	kajians, err := h.kajianService.GetAll()
+	if err != nil {
+		response := helper.FailedResponse1(http.StatusBadRequest, err.Error(), err)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
 
+	response := helper.SuccessfulResponse1(kajians)
+	c.JSON(http.StatusOK, response)
 }
