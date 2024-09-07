6eae564f6d5727f7ba980f84811f5c77e0f546c4
secona <77039267+secona@users.noreply.github.com>
2024-09-07 11:24:04 +0700

    feat: image uploads for kajian

--- handler/kajian.go
+++ handler/kajian.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"greenwelfare/dto"
 	"greenwelfare/helper"
+	"greenwelfare/imagekits"
 	"greenwelfare/service"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,7 +23,51 @@ func NewKajianHandler() *kajianHandler {
 }
 
 func (h *kajianHandler) CreateKajian(c *gin.Context) {
-	// TODO: upload images
+	var imagesKitURLs []string
+
+	for i := 1; ; i++ {
+		fileKey := fmt.Sprintf("file%d", i)
+		file, err := c.FormFile(fileKey)
+		
+		// If there are no more files to upload, break the loop
+		if err == http.ErrMissingFile {
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("Error when opening file %s: %v\n", fileKey, err)
+			continue // Skip to the next file
+		}
+
+		src, err := file.Open()
+		if err != nil {
+			fmt.Printf("Error when opening file %s: %v\n", fileKey, err)
+			continue
+		}
+		defer src.Close()
+
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, src); err != nil {
+			fmt.Printf("Error reading file %s: %v\n", fileKey, err)
+			continue
+		}
+
+		img, err := imagekits.Base64toEncode(buf.Bytes())
+		if err != nil {
+			fmt.Printf("Error reading image %s: %v\n", fileKey, err)
+			continue
+		}
+
+		fmt.Printf("Image base64 format %s: %v\n", fileKey, img)
+
+		imageKitURL, err := imagekits.ImageKit(context.Background(), img)
+		if err != nil {
+			fmt.Printf("Error uploading image %s to ImageKit: %v\n", fileKey, err)
+			continue
+		}
+
+		imagesKitURLs = append(imagesKitURLs, imageKitURL)
+	}
 
 	var input dto.InputKajian
 	if err := c.ShouldBind(&input); err != nil {
@@ -32,6 +81,14 @@ func (h *kajianHandler) CreateKajian(c *gin.Context) {
 		response := helper.FailedResponse1(http.StatusBadRequest, err.Error(), err)
 		c.JSON(http.StatusBadRequest, response)
 		return
+	}
+
+	for _, url := range imagesKitURLs {
+		if err := h.kajianService.CreateImage(kajian.ID, url); err != nil {
+			response := helper.APIresponse(http.StatusUnprocessableEntity, err.Error())
+			c.JSON(http.StatusUnprocessableEntity, response)
+			return
+		}
 	}
 
 	response := helper.SuccessfulResponse1(kajian)
